c735288bc160885e01ef5b41997f9680f7e23a38
jean-baptiste quatravaux <jean-baptistequatravaux@MacBook-Pro-6.local>
2026-03-18 16:47:41 +0100

    Fix GitHub Pages router basename

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<OptinPage />} />
           <Route path="/offre" element={<OffrePage />} />
